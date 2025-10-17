f043d55a205a5f6a1020cafec77d2056770bba39
hs1130 <whgustj1130@naver.com>
2025-10-17 11:31:00 +0900

    기업형 웹프로젝트(밀양재단)

---
+++ sub6/js/map_one.js
@@ -0,0 +1,27 @@
+ window.onload = function() {
+    var container = document.getElementById('map');
+    var options = {
+        center: new daum.maps.LatLng(37.63297790427136, 126.83129584988328),
+        level: 3
+    };
+
+    var map = new daum.maps.Map(container, options);
+    
+    var mapTypeControl = new daum.maps.MapTypeControl();
+    map.addControl(mapTypeControl, daum.maps.ControlPosition.TOPRIGHT);
+
+    var zoomControl = new daum.maps.ZoomControl();
+    map.addControl(zoomControl, daum.maps.ControlPosition.RIGHT);
+    
+    var markerPosition  = new daum.maps.LatLng(37.5665071, 126.9794138); 
+    var marker = new daum.maps.Marker({
+        position: markerPosition
+    });
+
+    marker.setMap(map);
+    
+    var overlay = new daum.maps.CustomOverlay({
+        map: map,
+        position: marker.getPosition()       
+    });
+}
